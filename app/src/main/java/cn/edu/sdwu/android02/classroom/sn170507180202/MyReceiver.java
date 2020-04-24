b02c09f494c26fa5f05619e72c334a7fcff5e553
170507180202 <2048606614@qq.com>
2020-04-24 15:10:11 +0800

    增加新代码

--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180202/MyReceiver.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180202/MyReceiver.java
@@ -1,18 +1,35 @@
 package cn.edu.sdwu.android02.classroom.sn170507180202;
 
 import android.content.BroadcastReceiver;
+
 import android.content.Context;
+
 import android.content.Intent;
+
 import android.widget.Toast;
+
+
 
 public class MyReceiver extends BroadcastReceiver {
 
+
+
     @Override
+
     public void onReceive(Context context, Intent intent) {
-        //当接受到广播时，自动调用类方法
+
+        //当接收广播时自动调用本方法
+
         // TODO: This method is called when the BroadcastReceiver is receiving
+
         // an Intent broadcast.
+
+        //获取广播内容
+
         String content=intent.getStringExtra("key1");
-        Toast.makeText(context,content,Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(context,content, Toast.LENGTH_SHORT).show();
+
     }
+
 }
