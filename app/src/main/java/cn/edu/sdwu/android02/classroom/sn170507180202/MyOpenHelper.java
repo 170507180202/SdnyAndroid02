d246ba69fc3f99f134354bb2303b0388b3669c55
170507180202 <2048606614@qq.com>
2020-05-06 21:25:30 +0800

    增加新代码

--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180202/MyOpenHelper.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180202/MyOpenHelper.java
@@ -1,34 +1,34 @@
 package cn.edu.sdwu.android02.classroom.sn170507180202;
 
+import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
-
 /**
- * Created by Administrator on 2020/4/29 0029.
+ * Created by dell on 2020/4/29.
  */
 
 public class MyOpenHelper extends SQLiteOpenHelper {
-    private MyOpenHelper myOpenHelper;
-    private String STUDENT_TB_SQL="creat table student(id integer primary key autoincrement,stuname text,stutel text)";
+    private String STUDENT_TB_SQL="create table student(id integer primary  key autoincrement,stuname text,stutel text)";
     public MyOpenHelper(Context context){
-        //Context context上下文 String name数据库名称 CursorFctory传入MULL int version数据库的版本
-        super(context,"stud.db",null,2);
+        //Context context上下文，String name数据库名称，CursorFactory factory传入NULL，int version 数据库的版本
+        super(context,"stud.db",null,2);//建立一个学生的数据库stud.db
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        //当打开数据库时候 发现数据库并不存在 此时会自动创建数据库 然后调用oncreat 方法
-        //在本方法中完成数据库对象的创建 比如表 视图 索引等
-        //通过SQLLiteDatabase对象 完成SQL的执行
+        //当打开数据库时，数据库并不存在，此时会自动创建数据库，然后调用onCreate方法
+        //在本方法，完成数据库对象（表，视图，索引等）的创建
+        //通过sqLiteDatabase对象，执行SQL
         sqLiteDatabase.execSQL(STUDENT_TB_SQL);
-        Log.i(MyOpenHelper.class.toString(),"onCreate.");
-
-
+        Log.i(MyOpenHelper.class.toString(),"onCreate");
     }
+
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("alter table student add column stuadd text");
+        //当构造方法中制定的版本号，与手机中已有数据库的版本号更新的时候调用本方法
+        sqLiteDatabase.execSQL("alter table student and column stuadd text");
     }
 }
+
--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180202/MyService2.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180202/MyService2.java
@@ -10,13 +10,12 @@ import java.util.Random;
 
 public class MyService2 extends Service {
     private Random random;
-    private  MyBinder myBinder;
+    private MyBinder myBinder;
 
     @Override
     public void onCreate() {
         super.onCreate();
         random=new Random();
-        myBinder=new MyBinder();
     }
 
     public MyService2() {
@@ -24,7 +23,7 @@ public class MyService2 extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        //返回值 会返回给调用者，以后调用者与服务进行交互，都会使用此返回值
+        //返回值，会返回给调用者与服务进行交互，都会使用此返回值
         // TODO: Return the communication channel to the service.
         Log.i(MyService2.class.toString(),"onBind");
         return myBinder;
@@ -38,19 +37,20 @@ public class MyService2 extends Service {
 
     @Override
     public void onDestroy() {
-        //当所有者解绑后，销毁
+        //当所有调用者都解绑，销毁
         Log.i(MyService2.class.toString(),"onDestroy");
         super.onDestroy();
     }
 
     public class MyBinder extends Binder{
-        public  MyService2 getRandomService(){
-            return MyService2.this;
+        public MyService2 getRandomService(){
 
+            return MyService2.this;
         }
     }
+
     //实际与调用者进行交互的方法
-    public int genRandom(){
+    public int getRandom(){
         return random.nextInt();
     }
 }
